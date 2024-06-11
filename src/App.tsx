d02de93e2d9fc4e72ccf81a79371e55ecf5ca9b2
Husnain Arshad <husnain@openletterconnect.com>
2024-06-11 20:53:17 +0500

    Added: UI Components

--- src/App.tsx
+++ src/App.tsx
@@ -35,51 +35,13 @@ function App() {
 
   return (
     <>
-      {/* <TemplateBuilder apiKey="aqsqwlksjddk" secret="ysCpKe5xuksqn5IdNqHJ" /> */}
+      <TemplateBuilder apiKey="aqsqwlksjddk" secret="ysCpKe5xuksqn5IdNqHJ" />
       <Button onClick={handleOpen} style={buttonStyles}>
         Button
       </Button>
-      <Typography variant="p" style={typoStyles}>
-        Typography
-      </Typography>
+      <Typography variant="p" style={typoStyles}>Typography</Typography>
       <CircularProgress />
-      <Dialog
-        customStyles={dialogStyles}
-        open={open}
-        handleClose={handleClose}
-        title="Delete Template"
-        subHeading="Are you sure you want to delete this Template?"
-        desscription="This template will be deleted from your Templates list but will
-            still be included in associated orders."
-        onSubmit={() => {}}
-        onCancel={handleClose}
-        cancelText="Cancel"
-        submitText="Submit"
-      />
-      <div>
-        <GridContainer>
-          <GridItem xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Typography variant="p" style={typoStyles}>
-              Item 1
-            </Typography>
-          </GridItem>
-          <GridItem xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Typography variant="p" style={typoStyles}>
-              Item 2
-            </Typography>
-          </GridItem>
-          <GridItem xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Typography variant="p" style={typoStyles}>
-              Item 3
-            </Typography>
-          </GridItem>
-          <GridItem xs={12} sm={6} md={4} lg={3} xl={3}>
-            <Typography variant="p" style={typoStyles}>
-              Item 4
-            </Typography>
-          </GridItem>
-        </GridContainer>
-      </div>
+      <Dialog customStyles={dialogStyles} open={open} handleClose={handleClose} />
     </>
   );
 }
--- src/components/GenericUIBlocks/Typography/index.tsx
+++ src/components/GenericUIBlocks/Typography/index.tsx
@@ -4,9 +4,26 @@ import React from 'react';
 import './styles.scss'
 
 const Typography = ({children="", style={}, variant=""}: any) => {
+<<<<<<< HEAD
   const Tag = variant || 'p';
   return (
     <Tag style={style}>{children}</Tag>
+=======
+  return (
+    <>
+      {variant === 'h1' && <h1 className="basic-typo">{children}</h1>}
+      {variant === 'h2' && <h2 className="basic-typo">{children}</h2>}
+      {variant === 'h3' && <h3 className="basic-typo">{children}</h3>}
+      {variant === 'h4' && <h4 className="basic-typo">{children}</h4>}
+      {variant === 'h5' && <h5 className="basic-typo">{children}</h5>}
+      {variant === 'h6' && <h6 className="basic-typo">{children}</h6>}
+      {variant === 'p' && 
+      <p className="basic-typo" style={style}>
+        {children}
+      </p>
+      }
+    </>
+>>>>>>> 706b4bc (Added: UI Components)
   );
 };
 
