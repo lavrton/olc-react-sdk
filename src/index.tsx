7e102d1ae756bb198bbb039abe8bc9d9770d3b98
Anton Lavrenov <lavrton@gmail.com>
2024-09-03 12:57:10 -0500

    Destroy editor function

--- src/index.tsx
+++ src/index.tsx
@@ -87,6 +87,12 @@ const TemplateBuilder = ({
       </Provider>
     </>
   );
+
+  return {
+    destroy() {
+      root.unmount();
+    }
+  }
 };
 
 // Example to run the project locally for development. Comment out these lines when building the application
